b19d03d8bc368b7fea5d8c93d92a05b68a072816
Jeff Knaggs <mail@jeef3.com>
2014-03-24 21:34:56 +1300

    Fixing remover

--- remover.js
+++ remover.js
@@ -9,13 +9,20 @@ module.exports = function (sftp) {
     var unlinkDeferreds = [];
 
     files.forEach(function (file) {
+      var toRemove = file.path + '/' + file.filename;
+      console.log('Deleting remote file:', toRemove);
+
       unlinkDeferreds
-        .push(Q.nfcall(sftp.unlink, file.path + '/' + file.filename));
+        .push(Q.nfcall(sftp.unlink, toRemove));
     });
 
     Q.all(unlinkDeferreds)
       .then(function () {
         deferred.resolve();
+      }, function (err) {
+        if (err) {
+          throw err;
+        }
       });
 
     return deferred.promise;
